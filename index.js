04d7da05c2a0cb300c2ea6dc5596703ae159d0d8
Florian Beck <florian.f.beck@web.de>
2021-11-27 23:04:57 +0100

    Existing code implemented with new code

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+'use strict'; // eslint-disable-line
+
 /**
  * General events
  */
@@ -5,6 +7,7 @@ const tryOutButton = document.getElementById('try-out');
 const learnMoreButton = document.getElementById('learn-more');
 const sections = Array.from(document.getElementsByTagName('section'));
 const [infoBox, keyboardDemo] = sections;
+const input = keyboardDemo.firstElementChild;
 
 tryOutButton.addEventListener('click', () => {
   if (infoBox.classList.contains('hidden')) {
@@ -20,7 +23,9 @@ tryOutButton.addEventListener('click', () => {
     tryOutButton.classList.add('go-back');
     tryOutButton.lastElementChild.innerHTML = 'Go back';
 
-    infoBox.focus(); // needs debugging
+    input.focus();
+    input.value = '';
+    // FIXME: add max character count to input field
   }
 });
 
@@ -28,83 +33,76 @@ learnMoreButton.addEventListener('click', () => {
   window.location='https://github.com/qwerty-fr/qwerty-fr#readme';
 });
 
-
+/**
+ * Keyboard emulation
+ */
 
 window.addEventListener('DOMContentLoaded', () => {
-  'use strict'; // eslint-disable-line
-
   const keyboard = document.querySelector('x-keyboard');
-  const input = document.querySelector('input');
-  const demo = document.querySelector('#demo');
-
+  
   if (!keyboard.layout) {
-    console.warn('web component x-keyboard couldn\'t be loaded');
-    return; // the web component has not been loaded
-  }
-
-  fetch(`layouts/qwerty-fr.json`)
+    console.warn('web component x-keyboard couldn\'t be loaded'); // the web component has not been loaded
+  } else {
+    fetch(`layouts/qwerty-fr.json`)
     .then(response => response.json())
     .then(data => {
       // FIXME: decide based on user keyboard
       const shape = 'iso';
       keyboard.setKeyboardLayout(data.keymap, data.deadkeys, shape);
     });
-
-  document.body.classList.add('demo');
-  demo.hidden = false;
-  input.value = '';
-  input.focus();
-
-  /**
-   * Keyboard highlighting & layout emulation
-   */
-
-  // required to work around a Chrome bug, see the `keyup` listener below
-  const pressedKeys = {};
-
-  // highlight keyboard keys and emulate the selected layout
-  input.onkeydown = (event) => {
-    pressedKeys[event.code] = true;
-    const value = keyboard.keyDown(event);
-    if (value) {
-      event.target.value += value;
-    } else if (event.code === 'Enter') { // clear text input on <Enter>
-      event.target.value = '';
-    } else if ((event.code === 'Tab') || (event.code === 'Escape')) {
-      setTimeout(close, 100);
-    } else {
-      return true; // don't intercept special keys or key shortcuts
-    }
-    return false; // event has been consumed, stop propagation
-  };
-  input.addEventListener('keyup', (event) => {
-    if (pressedKeys[event.code]) { // expected behavior
-      keyboard.keyUp(event);
-      delete pressedKeys[event.code];
-    } else {
-      /**
-       * We got a `keyup` event for a key that did not trigger any `keydown`
-       * event first: this is a known bug with "real" dead keys on Chrome.
-       * As a workaround, emulate a keydown + keyup. This introduces some lag,
-       * which can result in a typo (especially when the "real" dead key is used
-       * for an emulated dead key) -- but there's not much else we can do.
-       */
-      event.target.value += keyboard.keyDown(event);
-      setTimeout(() => keyboard.keyUp(event), 100);
-    }
-  });
-
-  /**
-   * When pressing a "real" dead key + key sequence, Firefox and Chrome will
-   * add the composed character directly to the text input (and nicely trigger
-   * an `insertCompositionText` or `insertText` input event, respectively).
-   * Not sure wether this is a bug or not -- but this is not the behavior we
-   * want for a keyboard layout emulation. The code below works around that.
-   */
-  input.addEventListener('input', (event) => {
-    if (event.inputType === 'insertCompositionText'
-      || event.inputType === 'insertText') {
-      event.target.value = event.target.value.slice(0, -event.data.length);
-    }
-  });
+  
+    /**
+     * Keyboard highlighting & layout emulation
+     */
+  
+    // required to work around a Chrome bug, see the `keyup` listener below
+    const pressedKeys = {};
+  
+    // highlight keyboard keys and emulate the selected layout
+    input.addEventListener('keydown', (event) => {
+      pressedKeys[event.code] = true;
+      const value = keyboard.keyDown(event);
+      if (value) {
+        event.target.value += value;
+      } else if (event.code === 'Enter') { // clear text input on <Enter>
+        event.target.value = '';
+      } else if ((event.code === 'Tab') || (event.code === 'Escape')) {
+        setTimeout(close, 100);
+      } else {
+        return true; // don't intercept special keys or key shortcuts
+      }
+      return false; // event has been consumed, stop propagation
+    });
+  
+    input.addEventListener('keyup', (event) => {
+      if (pressedKeys[event.code]) { // expected behavior
+        keyboard.keyUp(event);
+        delete pressedKeys[event.code];
+      } else {
+        /**
+         * We got a `keyup` event for a key that did not trigger any `keydown`
+         * event first: this is a known bug with "real" dead keys on Chrome.
+         * As a workaround, emulate a keydown + keyup. This introduces some lag,
+         * which can result in a typo (especially when the "real" dead key is used
+         * for an emulated dead key) -- but there's not much else we can do.
+         */
+        event.target.value += keyboard.keyDown(event);
+        setTimeout(() => keyboard.keyUp(event), 100);
+      }
+    });
+  
+    /**
+     * When pressing a "real" dead key + key sequence, Firefox and Chrome will
+     * add the composed character directly to the text input (and nicely trigger
+     * an `insertCompositionText` or `insertText` input event, respectively).
+     * Not sure wether this is a bug or not -- but this is not the behavior we
+     * want for a keyboard layout emulation. The code below works around that.
+     */
+    input.addEventListener('input', (event) => {
+      if (event.inputType === 'insertCompositionText'
+        || event.inputType === 'insertText') {
+        event.target.value = event.target.value.slice(0, -event.data.length);
+      }
+    });
+  }
 });
